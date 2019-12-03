0b7b7cb3cf1462254f004afa216bd29178617093
Gabriel Civinski <gabrielcivinski@gmail.com>
2019-12-02 21:48:25 -0300

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+    aws_accessKeyId: "AKIASVYMSKI3GIICGXAL",
+    aws_secretAccessKey: "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",
+};
+
+module.exports = config;
---
+++ src/database/index.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk');
+const config = require('../config/index');
+
+console.log(config.aws_accessKeyId);
+console.log(config.aws_secretAccessKey);
+
+AWS.config.update({
+ "region": "us-east-2",
+ "accessKeyId": config.aws_accessKeyId,
+ "secretAccessKey": config.aws_secretAccessKey,
+});
+
+const dynamoConnection = new AWS.DynamoDB.DocumentClient();
+
+module.exports = dynamoConnection;
+
